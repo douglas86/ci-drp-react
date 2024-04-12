14ef8769e6f30a6263eed85d2308d4554b265f2d
douglas <douglasmaxton@gmail.com>
2024-04-12 11:22:57 +0100

    updated: App, Button and SignInUpForm style sheets

--- src/constructors/App.jsx
+++ src/constructors/App.jsx
@@ -1,31 +1,39 @@
 // third party libraries
-import Container from 'react-bootstrap/Container'
-import {Route, Routes} from 'react-router-dom'
+import Container from "react-bootstrap/Container";
+import { Route, Routes } from "react-router-dom";
 
 // constructors directory
-import {router} from "./router";
+import { router } from "./router";
 
 // components directory
 import NavBar from "../components/templates/NavBar";
 
+// api directory
+import "../api/axiosDefaults";
+
 // styling
-import styles from '../styles/App.module.css'
+import styles from "../styles/App.module.css";
 
 const App = () => {
-    return (
-        <div className={styles.App}>
-            <NavBar/>
-            <Container className={styles.Main}>
-                <Routes>
-                    <>
-                        {router.map((route, index) => (
-                            <Route exact key={index} path={route.path} element={route.element}/>
-                        ))}
-                    </>
-                </Routes>
-            </Container>
-        </div>
-    )
-}
+  return (
+    <div className={styles.App}>
+      <NavBar />
+      <Container className={styles.Main}>
+        <Routes>
+          <>
+            {router.map((route, index) => (
+              <Route
+                exact
+                key={index}
+                path={route.path}
+                element={route.element}
+              />
+            ))}
+          </>
+        </Routes>
+      </Container>
+    </div>
+  );
+};
 
 export default App;
