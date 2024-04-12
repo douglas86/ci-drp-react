d8d3624699738bc6979ae71d57b2ec3baecd5291
douglas <douglasmaxton@gmail.com>
2024-04-12 11:31:18 +0100

    changed: sign out to signup on navbar

--- src/constructors/router.jsx
+++ src/constructors/router.jsx
@@ -1,31 +1,31 @@
-import Home from '../components/pages/Home'
+import Home from "../components/pages/Home";
 import SignIn from "../components/pages/SignIn";
-import SignOut from "../components/pages/SignOut";
 import NotFound from "../components/pages/NotFound";
+import SignUpForm from "../components/pages/auth/SignUpForm";
 
 export const router = [
-    {
-        path: '/',
-        title: 'Home',
-        element: <Home/>,
-        fontawesomeIcon: 'fa-home',
-    },
-    {
-        path: '/signin',
-        title: 'Sign In',
-        element: <SignIn/>,
-        fontawesomeIcon: 'fa-sign-in-alt',
-    },
-    {
-        path: '/signout',
-        title: 'Sign Out',
-        element: <SignOut/>,
-        fontawesomeIcon: 'fa-user-plus',
-    },
-    {
-        path: '*',
-        title: 'None',
-        element: <NotFound/>,
-        fontawesomeIcon: '',
-    }
-]
+  {
+    path: "/",
+    title: "Home",
+    element: <Home />,
+    fontawesomeIcon: "fa-home",
+  },
+  {
+    path: "/signin",
+    title: "Sign In",
+    element: <SignIn />,
+    fontawesomeIcon: "fa-sign-in-alt",
+  },
+  {
+    path: "/signup",
+    title: "Sign Up",
+    element: <SignUpForm />,
+    fontawesomeIcon: "fa-user-plus",
+  },
+  {
+    path: "*",
+    title: "None",
+    element: <NotFound />,
+    fontawesomeIcon: "",
+  },
+];
