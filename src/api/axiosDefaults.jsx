196641967d3f4ed29a5f706b889da49b010d7b48
Douglas Maxton <douglasmaxton@gmail.com>
2024-04-19 12:09:33 +0000

    changed: localhost to heroku axios default

--- src/api/axiosDefaults.jsx
+++ src/api/axiosDefaults.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-axios.defaults.baseURL = "http://localhost:8000";
+axios.defaults.baseURL = "https://ci-drp-api-f5b03f3c5f4f.herokuapp.com/";
 axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
 axios.defaults.withCredentials = true;
 
